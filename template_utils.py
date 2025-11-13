f71a9ecd971cc8c927fe6375870095a5feca7120
mnasirintpmc-cell <mnasirintpmc@gmail.com>
2025-11-13 20:49:04 +0000

    Create template_utils.py

---
+++ template_utils.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def create_smart_template():
+    """Create Excel-friendly template"""
+    smart_template = {
+        'TST_SpeedDem': ['SET_SPEED'],
+        'TST_CellPresDemand': ['SET_PRESSURE'],
+        'TST_InterPresDemand': ['SET_PRESSURE'],
+        'TST_InterBPDemand_DE': ['SET_PRESSURE'],
+        'TST_InterBPDemand_NDE': ['SET_PRESSURE'],
+        'TST_GasInjectionDemand': ['SET_PRESSURE'],
+        'TST_StepDuration': ['SET_DURATION'],
+        'TST_APFlag': ['SET_FLAG'],
+        'TST_TempDemand': ['SET_TEMPERATURE'],
+        'TST_GasType': ['Air'],
+        'TST_TestMode': ['SET_MODE'],
+        'TST_MeasurementReq': ['SET_MEASURE'],
+        'TST_TorqueCheck': [0]
+    }
+    return pd.DataFrame(smart_template)
+
+def create_example_sequence():
+    """Create example based on real test patterns"""
+    example_sequence = {
+        'TST_SpeedDem': [0, 0, 0, 3600, 3600, 0],
+        'TST_CellPresDemand': [0.21, 1.0, 10.0, 5.0, 20.0, 0.5],
+        'TST_InterPresDemand': [0, 0, 0, 0, 0, 0],
+        'TST_InterBPDemand_DE': [0, 0, 0, 1.0, 0.4, 0],
+        'TST_InterBPDemand_NDE': [0, 0, 0, 1.0, 0.4, 0],
+        'TST_GasInjectionDemand': [0, 0, 0, 0, 0, 0],
+        'TST_StepDuration': [2, 2, 2, 2, 10, 2],
+        'TST_APFlag': [0, 0, 0, 0, 1, 0],
+        'TST_TempDemand': [30, 30, 30, 155, 155, 155],
+        'TST_GasType': ['Air', 'Air', 'Air', 'Air', 'Air', 'Air'],
+        'TST_TestMode': [1, 1, 1, 1, 1, 1],
+        'TST_MeasurementReq': [1, 1, 1, 1, 1, 1],
+        'TST_TorqueCheck': [0, 0, 0, 0, 0, 0]
+    }
+    return pd.DataFrame(example_sequence)
