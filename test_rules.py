a880167d5277766324ff4d9a7e8faafe9739379a
mnasirintpmc-cell <mnasirintpmc@gmail.com>
2026-03-06 14:12:24 +0000

    Update test_rules.py

--- test_rules.py
+++ test_rules.py
@@ -1,34 +1,17 @@
+# =====================================================
+# DEFAULT TEST RULES
+# =====================================================
+
 TEST_RULES = {
 
-    "Primary Cold Static": {
-        "rpm": 0,
-        "temperature": "AMB",
-        "mode": 1
-    },
+    "default": {
 
-    "Secondary Cold Static": {
-        "rpm": 0,
-        "temperature": "AMB",
-        "mode": 2
-    },
+        "Gas_Type": "Air",
+        "Gas_Injection_bar": 0,
+        "Measurement": 0,
+        "Torque_Check": 0,
+        "Test_Mode": 1
 
-    "MRT Test": {
-        "rpm": 0,
-        "temperature": ">160",
-        "mode": 1,
-        "torque": 1
-    },
-
-    "Primary Dynamic": {
-        "temperature": ">160",
-        "secondary_fixed": 1,
-        "mode": 1
-    },
-
-    "Primary Hot Static": {
-        "rpm": 0,
-        "temperature": ">160",
-        "mode": 1
     }
 
 }
