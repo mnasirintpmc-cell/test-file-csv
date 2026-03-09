45eb66efc70279e6190170a706f14a60def9dbc0
mnasirintpmc-cell <mnasirintpmc@gmail.com>
2026-03-09 08:52:57 +0000

    Update test_rules.py

--- test_rules.py
+++ test_rules.py
@@ -1,17 +1,8 @@
-# =====================================================
-# DEFAULT TEST RULES
-# =====================================================
+DEFAULT_VALUES = {
 
-TEST_RULES = {
-
-    "default": {
-
-        "Gas_Type": "Air",
-        "Gas_Injection_bar": 0,
-        "Measurement": 0,
-        "Torque_Check": 0,
-        "Test_Mode": 1
-
-    }
+    "Gas_Type": "Air",
+    "Gas_Injection_bar": 0,
+    "Torque_Check": 0,
+    "Measurement": 0
 
 }
